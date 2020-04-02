11b2ad813c09a27ea486a7f8bcd69f73c823a1dc
Lockett Pundt <kalifornium@hotmail.com>
2020-04-02 13:13:44 -0400

    add the require cors and corsOptions to app.

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
-const express = require('express'),
-    session = require('express-session'),
-    FileStore = require('session-file-store')(session),
-    path = require('path'),
-    bodyParser = require('body-parser'),
-    cookieParser = require('cookie-parser'),
-    es6Renderer = require('express-es6-template-engine'),
-    logger = require('morgan');
-
+const express = require('express');
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
+const path = require('path');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const es6Renderer = require('express-es6-template-engine');
+const logger = require('morgan');
+const cors = require('cors');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
-const recipelistRouter = require('./routes/recipelist')
+const recipelistRouter = require('./routes/recipelist');
 
+const corsOptions = {
+  origin: '*',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Headers':
+      'Origin, X-Requested-With, Content-Type, Accept',
+};
 
 const app = express();
 app.engine('html', es6Renderer);
@@ -22,15 +31,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors(corsOptions));
 
-
-app.use(session( {
-    store: new FileStore(),
-    secret: 'secret',
-    resave: false,
-    saveUninitialized: true,
-    is_logged_in: false
-}))
+app.use(session({
+  store: new FileStore(),
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: true,
+  is_logged_in: false,
+}));
 
 
 app.use('/', indexRouter);
