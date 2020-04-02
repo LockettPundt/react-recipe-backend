59545e430f1116ab3c98f524e13754cc8ded1e7b
Lockett Pundt <kalifornium@hotmail.com>
2020-04-02 13:56:55 -0400

    removed the cors since it was blocking the api call?

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 
 app.use(session({
   store: new FileStore(),
