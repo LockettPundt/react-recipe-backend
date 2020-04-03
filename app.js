adb53033b10b0b19fc704d1b914ac8be3891fb34
Lockett Pundt <kalifornium@hotmail.com>
2020-04-02 20:26:45 -0400

    removed non essential requires and render methods.

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 const express = require('express');
-const session = require('express-session');
-const FileStore = require('session-file-store')(session);
 const path = require('path');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const es6Renderer = require('express-es6-template-engine');
 const logger = require('morgan');
 const cors = require('cors');
 const indexRouter = require('./routes/index');
@@ -18,33 +15,16 @@ const corsOptions = {
   optionsSuccessStatus: 204,
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Headers':
-      'Origin, X-Requested-With, Content-Type, Accept',
+    'Origin, X-Requested-With, Content-Type, Accept',
 };
-
 const app = express();
-app.engine('html', es6Renderer);
-app.set('views', './views');
-app.set('view engine', 'html');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-// app.use(cors(corsOptions));
-
-app.use(session({
-  store: new FileStore(),
-  secret: 'secret',
-  resave: false,
-  saveUninitialized: true,
-  is_logged_in: false,
-}));
-
-
+app.use(cors(corsOptions));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/recipelist', recipelistRouter);
-
-
 module.exports = app;
